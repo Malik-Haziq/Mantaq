86fb7880ae25cf804fe7d6668ba9b76fbfd9dc5c
Malik-Haziq <malikhaziq153@gmail.com>
2023-11-16 10:40:04 -0800

    theme set to light

--- script.js
+++ script.js
@@ -73,20 +73,20 @@ cards.addEventListener("mousemove", (e) => {
 const switchThemeEl = document.querySelector('input[type="checkbox"]');
 const storedTheme = localStorage.getItem('theme')
 
-switchThemeEl.checked = storedTheme === 'dark' || storedTheme === null
+switchThemeEl.checked = storedTheme === 'light' || storedTheme === null
 
 switchThemeEl.addEventListener("click", () => {
   const isChecked = switchThemeEl.checked;
 
   if (!isChecked) {
-    document.body.classList.remove("dark");
-    document.body.classList.add("light");
-    localStorage.setItem("theme", "light");
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
     switchThemeEl.checked = false
   } else {
-    document.body.classList.add("dark");
-    document.body.classList.remove("light");
-    localStorage.setItem("theme", "dark");
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+    localStorage.setItem("theme", "light");
   }
 
 });
